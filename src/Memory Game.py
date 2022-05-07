ab0c9491972e4d591c66fb7558ac5a9e4e7c74c0
Ryan <rdabrunk@gmail.com>
2022-05-07 18:17:17 -0400

    implemented method to draw a four by four grid of squares

--- src/Memory Game.py
+++ src/Memory Game.py
@@ -1,8 +1,9 @@
-
+import numpy as np
 import pygame
 from pygame.locals import *
 import random
 from turtle import screensize
+import numpy
 
 class MemoryGame:
 
@@ -10,7 +11,7 @@ class MemoryGame:
         pygame.init()
         pygame.display.set_caption("Memory Game")
 
-        self.black, self.white = (0, 0, 0), (255, 255, 255)
+        self.black, self.white, self.blue = (0, 0, 0), (255, 255, 255), (0, 0, 255)
         self.screensize = screensize
         self.screenrect = pygame.Rect(0, 0, self.screensize[0], self.screensize[1])
         self.screen = pygame.display.set_mode(self.screensize)
@@ -21,8 +22,17 @@ class MemoryGame:
                 pygame.quit()
                 exit()
 
+    def draw_squares(self):
+        square_x_positions = np.linspace(self.screensize[0] * 0.15 ,self.screensize[0] * 0.8, 4)
+        square_y_positions = np.linspace(self.screensize[1] * 0.2, self.screensize[1] * 0.8, 4)
+        for i in range(4):
+            for j in range(4):
+                pygame.draw.rect(self.screen, self.blue, (square_x_positions[i], square_y_positions[j], 60,60))
+
     def loop(self):
         self.handle_events()
+        self.draw_squares()
+        pygame.display.update()
 
     def run(self):
         while True:
