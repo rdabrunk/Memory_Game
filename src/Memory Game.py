95312cd3a8a6a26fc97a0c7d02406fe301a944a1
Ryan <rdabrunk@gmail.com>
2022-05-08 22:13:15 -0400

    implemented scoreboard, game now functions

--- src/Memory Game.py
+++ src/Memory Game.py
@@ -12,15 +12,17 @@ class MemoryGame:
         pygame.init()
         pygame.display.set_caption("Memory Game")
 
-        self.black, self.white, self.blue, self.red = (0, 0, 0), (255, 255, 255), (0, 0, 255), (255, 0, 0)
+        self.black, self.white, self.blue, self.red, self.green = (0, 0, 0), (255, 255, 255), (0, 0, 255), (255, 0, 0), (0, 255, 0)
         self.screensize = screensize
         self.screenrect = pygame.Rect(0, 0, self.screensize[0], self.screensize[1])
         self.screen = pygame.display.set_mode(self.screensize)
         self.click_position = (-100, -100)
+        self.num_range = 1
+        self.score = 0
+        self.is_new_round = True
         self.get_coordinates()
         self.draw_squares()
-        pygame.display.update()
-        self.draw_round(2)
+        self.update_score()
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -35,6 +37,14 @@ class MemoryGame:
     def get_coordinates(self):
         self.x_positions = np.linspace(self.screensize[0] * 0.15 ,self.screensize[0] * 0.8, 4)
         self.y_positions = np.linspace(self.screensize[1] * 0.2, self.screensize[1] * 0.8, 4)
+
+    def update_score(self):
+        pygame.draw.rect(self.screen, self.black, (self.screensize[0] * .465, self.screensize[1] * 0.05, 120, 30))
+        pygame.display.update()
+        score_string = f"Score: {self.score}"
+        rendered_score = pygame.font.SysFont("Avenir", 36).render(score_string, True, self.white)
+        self.screen.blit(rendered_score, (self.screensize[0] * 0.465, self.screensize[1] * 0.05))
+        pygame.display.update()
 
 
     def draw_squares(self):
@@ -61,6 +71,7 @@ class MemoryGame:
             sleep(0.5)
         sleep(0.5)
         self.draw_squares()
+        pygame.display.update()
 
     def check_position(self):
         # print(self.click_position[0], self.click_position[1])
@@ -68,16 +79,29 @@ class MemoryGame:
         if len(self.coordinates) > 0:
             if (self.x_positions[self.coordinates[0][0]]) <= self.click_position[0] and (self.x_positions[self.coordinates[0][0]] + 60) >= self.click_position[0]:
                 if self.y_positions[self.coordinates[0][1]] <= self.click_position[1] and (self.y_positions[self.coordinates[0][1]] + 60) >= self.click_position[1]:
+                    pygame.draw.rect(self.screen, self.green, (self.x_positions[self.coordinates[0][0]], self.y_positions[self.coordinates[0][1]], 60,60))
+                    self.score += 1
+                    self.update_score()
+                    sleep(0.1)
+                    self.draw_squares()
+                    pygame.display.update()
                     print("yes")
                     self.coordinates.remove(self.coordinates[0])
             else:
                 print("no")
-        else:
-            print("coordinates is empty")
+                pygame.quit()
+                exit()
+        if len(self.coordinates) == 0:
+            self.is_new_round = not self.is_new_round
 
     def loop(self):
         self.handle_events()
-        pygame.display.update()
+        if self.is_new_round:
+            self.is_new_round = not self.is_new_round
+            self.draw_round(self.num_range)
+            self.num_range += 1
+
+
 
     def run(self):
         while True:
